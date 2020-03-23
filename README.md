878041bb37e12abb60fca952b40651c20a8c69b6
Nick Antoniou <nikolasea@windowslive.com>
2020-03-23 13:53:15 -0400

    Add README and split output in search method

---
+++ README.md
@@ -0,0 +1,4 @@
+# Spread of good programming practices
+
+Through this work, our aim is to investigate whether good programming practices are spread, on a large social network (such as Github and Gitlab), over time. This work uses the [WoC](http://www.mockus.us/papers/WoC.pdf) infrastructure.
+
--- code_quality_eval.py
+++ code_quality_eval.py
@@ -5,27 +5,34 @@ sys.path.insert(0, '../oscar.py')
 from oscar import Project
 import subprocess
 
-"""
-Process used to search for a specific blob, commit or tree.
-"""
 def search(hash, type):
+	"""
+	Method used to search for a specific blob, commit or tree.
+	
+	If a tree is searched for, the result is splitted into its components (blobs and directories),
+	which are again splitted into their mode, hash and name.
+	"""
 	proc = subprocess.Popen('echo ' + hash + ' | ~/lookup/showCnt ' + type, stdout=subprocess.PIPE, shell=True)
 	(out, err) = proc.communicate()
+	
+	if type == 'tree':
+		out = [blob.split(';') for blob in out.split()]
+
 	return out
 
 splitted_commit = search('0aea9614e05b94316380a29089a3c5a84d1232a1', 'commit').split()
 # the corresponding hash of the tree from a commit is always the second word
 tree_hash = splitted_commit[1]
 
-tree = search(tree_hash, 'tree')
-
-# split the result into its components (blobs and directories) and then separate the mode, the hash and the name
-blobs = [blob.split(';') for blob in tree.split()]
+blobs = search(tree_hash, 'tree')
 
 index = {'mode':1, 'hash':1, 'name':2}
 
 # files used in continuous integration
-ci_files = ['.gitlab-ci.yml', '.travis.yml', 'Jenkinsfile', 'buddy.yml', '.drone.yml', 'circle.yml', 'bamboo.yaml', 'codeship-steps.yml']
+ci_files = [
+	'.gitlab-ci.yml', '.travis.yml', 'Jenkinsfile', 'buddy.yml', '.drone.yml',
+	'circle.yml', 'bamboo.yaml', 'codeship-steps.yml'
+]
 
 # some tools use generic names for the configuration files, placing them in a distinct folder
 ci_config_folders = {'.circleci':'config.yml'}
