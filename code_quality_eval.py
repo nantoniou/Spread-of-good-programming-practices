e51c476be79b7bc12429e5e8e8e9eac5245ab80f
Nick Antoniou <nikolasea@windowslive.com>
2020-06-30 23:51:23 -0400

    Add .gitignore, add bash method and change calc_CI_introductions output into a file

--- code_quality_eval.py
+++ code_quality_eval.py
@@ -9,6 +9,11 @@ import subprocess
 from time import time as current_time
 start_time = current_time()
 
+def bash(command):
+	proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+	(out, err) = proc.communicate()
+	return out
+
 def search(hash, type):
 	"""
 	Method used to search for a specific blob, commit or tree.
@@ -19,9 +24,7 @@ def search(hash, type):
 	In the case of a commit, we split the information string and the tree hash and
 	parent's commit hash are returned
 	"""
-	proc = subprocess.Popen('echo ' + hash + ' | ~/lookup/showCnt ' + type,
-				stdout=subprocess.PIPE, shell=True)
-	(out, err) = proc.communicate()
+	out = bash('echo ' + hash + ' | ~/lookup/showCnt ' + type)
 	
 #	if type == 'tree':
 #		return [blob.split(';') for blob in out.strip().split('\n')]
@@ -44,8 +47,7 @@ def ci_lookup(tree_hash):
 		'shippable\.yml', 'phpci\.yml', 'cloudbuild\.yaml'
 	]
 	query = 'echo ' + tree_hash + ' | ~/lookup/showCnt tree | grep "' + '\|'.join(ci_files) +'"'
-	proc = subprocess.Popen(query, stdout=subprocess.PIPE, shell=True)
-	(out, err) = proc.communicate()
+	out = bash(query)
 	
 	"""
 	# alternate method
@@ -66,23 +68,26 @@ def ci_lookup(tree_hash):
 
 authors={}
 
-def calc_score(author):
+def calc_CI_introductions(author):
 	# getting the author's commits
-	proc = subprocess.Popen('echo "'+ author + '" | ~/lookup/getValues a2c',
-				stdout=subprocess.PIPE, shell=True)
-	(out, err) = proc.communicate()
+	out = bash('echo "'+ author + '" | ~/lookup/getValues a2c')
+
 	commits = [x for x in out.strip().split(';')[1:]]
 	
 	# using a dictionary that has the commits' hashes as keys,
 	# so as to not search multiple times for the same commit
 	CI_checked = {}
+
+	# delete contents
+	open('introductions.csv', 'w').close()
 	
 	# for every commit, we look up whether the author included a CI file,
 	# that did not exist in the parent commit
 	for count, commit in enumerate(commits):
+		# status update
 		if count % 50 == 0:
 			print count, ' / ', len(commits) 
-		
+	
 		tree_hash, parent_commit_hash, time = search(commit, 'commit')
 		if tree_hash not in CI_checked:
 			CI_checked[tree_hash] = ci_lookup(tree_hash)
@@ -93,9 +98,9 @@ def calc_score(author):
 			# controlling for the case of no parent commits
 			if parent == '':
 				break
-	
+		
 			parent_tree_hash = search(parent, 'commit')[0]
-	
+		
 			if parent_tree_hash not in CI_checked:
 				parent_CI = ci_lookup(parent_tree_hash)
 				CI_checked[parent_tree_hash] = parent_CI
@@ -107,10 +112,18 @@ def calc_score(author):
 		
 		# if the tree has a CI file, while the parent tree does not, increase the CI score
 		if CI_checked[tree_hash] and not all_parent_CI:
-			authors[author][0] += 1
 			
+			out = bash('echo ' + commit + ' | ~/lookup/getValues c2P')
+			main_proj = out.split(';')[1]
+			f = open("introductions.csv", "a")
+			f.write(author + ', ' + 'CI' + ', ' + str(time) + ', ' + main_proj)
+			f.close()
+			print 'wrote'
+			
+			"""	authors[author][0] += 1
+				
 			if authors[author][0] == 1 or time < authors[author][1]:
-				authors[author][1] = time
+				authors[author][1] = time"""
 			print authors
 
 	print (current_time()-start_time)/len(commits), 'seconds per commit'
@@ -121,9 +134,7 @@ def find_links(author, end_time, method='sh'):
 	affected the given author's use of good coding practices.
 	A timestamp is also given to define the time till which we find the connections.
 	"""
-	proc = subprocess.Popen('echo "'+ author + '" | ~/lookup/getValues a2p',
-				stdout=subprocess.PIPE, shell=True)
-	(out, err) = proc.communicate()
+	out = bash('echo "'+ author + '" | ~/lookup/getValues a2p')
 	pr = [x for x in out.strip().split(';')[1:]]
 	
 	#TODO handle forks
@@ -137,11 +148,11 @@ def find_links(author, end_time, method='sh'):
 
 # checking whether the user provided the author
 if len(sys.argv) > 1:
+	# initialize the number of CI integrations and the time
 	authors[sys.argv[1]] = [0, 0]
 else:
 	sys.exit('No author provided')
-
 for author in authors:
-	find_links(author, time.time())
-	#calc_score_tree_comp(author)
+	#find_links(author, current_time())
+	calc_CI_introductions(author)
 
